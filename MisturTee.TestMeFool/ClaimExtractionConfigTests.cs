71b215775f202eaa08b79cf6e21d0099c5496247
AbdulBeard <abdulkhaliqzaheer@gmail.com>
2018-03-19 11:57:17 -0400

    Finished Config->Claims UTs

--- MisturTee.TestMeFool/ClaimExtractionConfigTests.cs
+++ MisturTee.TestMeFool/ClaimExtractionConfigTests.cs
@@ -1,14 +1,105 @@
+using System;
+using System.Text.RegularExpressions;
+using MisturTee.Config;
+using MisturTee.Config.Claims;
 using MisturTee.Config.Claims.ExtractionConfigs;
+using MisturTee.Config.Claims.ExtractionConfigs.Valid;
 using Xunit;
 
 namespace MisturTee.TestMeFool
 {
     public class ClaimExtractionConfigTests
     {
+        [Fact]
         public void JsonPathClaimExtractionConfig()
         {
             var jsonPathClaimExtractionConfig = new JsonPathClaimExtractionConfig("PityTheFoolClaim");
-            Assert.Equal(1,1);
+            try
+            {
+                jsonPathClaimExtractionConfig.Build();
+                Assert.True(true);
+            }
+            catch (ArgumentException) { }
+            jsonPathClaimExtractionConfig.ConfigureExtraction(ExtractionFunctions.JsonPathFunc, null);
+            try
+            {
+                jsonPathClaimExtractionConfig.Build();
+                Assert.True(true);
+            }
+            catch (ArgumentException) { }
+            jsonPathClaimExtractionConfig.ConfigureExtraction(ExtractionFunctions.JsonPathFunc, "$.json");
+            var validJsonPathClaimExtractionConfig = jsonPathClaimExtractionConfig.Build();
+            Assert.Equal(ExtractionType.JsonPath, validJsonPathClaimExtractionConfig.ExtractionType);
+            Assert.Equal(ClaimLocation.Body, validJsonPathClaimExtractionConfig.ClaimLocation);
+        }
+
+        [Fact]
+        public void KeyValueClaimExtractionConfig()
+        {
+            var keyValueClaimExtractionConfig = new KeyValueClaimExtractionConfig("PityTheFoolClaim", ClaimLocation.Headers);
+            try
+            {
+                keyValueClaimExtractionConfig.Build();
+                Assert.True(true);
+            }
+            catch (ArgumentException) { }
+            keyValueClaimExtractionConfig.ConfigureExtraction(ExtractionFunctions.KeyValueFunc, null);
+            try
+            {
+                keyValueClaimExtractionConfig.Build();
+                Assert.True(true);
+            }
+            catch (ArgumentException) { }
+            keyValueClaimExtractionConfig.ConfigureExtraction(ExtractionFunctions.KeyValueFunc, "key");
+            var validJsonPathClaimExtractionConfig = keyValueClaimExtractionConfig.Build();
+            Assert.Equal(ExtractionType.KeyValue, validJsonPathClaimExtractionConfig.ExtractionType);
+            Assert.Equal(ClaimLocation.Headers, validJsonPathClaimExtractionConfig.ClaimLocation);
+        }
+
+        [Fact]
+        public void RegExClaimExtractionConfig()
+        {
+            var regexClaimExtractionConfig = new RegexClaimExtractionConfig("PityTheFoolClaim", ClaimLocation.Uri);
+            try
+            {
+                regexClaimExtractionConfig.Build();
+                Assert.True(true);
+            }
+            catch (ArgumentException) { }
+            regexClaimExtractionConfig.ConfigureExtraction(ExtractionFunctions.RegexFunc, null);
+            try
+            {
+                regexClaimExtractionConfig.Build();
+                Assert.True(true);
+            }
+            catch (ArgumentException) { }
+            regexClaimExtractionConfig.ConfigureExtraction(ExtractionFunctions.RegexFunc, new Regex("yolo/(.*)/solo"));
+            var validJsonPathClaimExtractionConfig = regexClaimExtractionConfig.Build();
+            Assert.Equal(ExtractionType.RegEx, validJsonPathClaimExtractionConfig.ExtractionType);
+            Assert.Equal(ClaimLocation.Uri, validJsonPathClaimExtractionConfig.ClaimLocation);
+        }
+
+        [Fact]
+        public void TypeClaimExtractionConfig()
+        {
+            var typeClaimExtractionConfig = new TypeClaimExtractionConfig<TestingType>("PityTheFoolClaim");
+            try
+            {
+                typeClaimExtractionConfig.Build();
+                Assert.True(true);
+            }
+            catch (ArgumentException) { }
+            typeClaimExtractionConfig.ConfigureExtraction(x => null);
+            try
+            {
+                typeClaimExtractionConfig.Build();
+                Assert.True(true);
+            }
+            catch (ArgumentException) { }
+            typeClaimExtractionConfig.ConfigureExtraction(x => null);
+            var validJsonPathClaimExtractionConfig = typeClaimExtractionConfig.Build();
+            Assert.Equal(ExtractionType.Type, validJsonPathClaimExtractionConfig.ExtractionType);
+            Assert.Equal(ClaimLocation.Body, validJsonPathClaimExtractionConfig.ClaimLocation);
         }
     }
 }
---
+++ MisturTee.TestMeFool/RouteClaimsConfigTest.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Internal;
+using MisturTee.Config.Claims;
+using MisturTee.Config.Claims.ExtractionConfigs.Valid;
+using MisturTee.Utils;
+using Xunit;
+
+namespace MisturTee.TestMeFool
+{
+    public class RouteClaimsConfigTest
+    {
+        [Fact]
+        public void BadRequestResponse()
+        {
+            dynamic response = new ExpandoObject();
+            response.ErrorCode = 1235;
+            response.Message = "The following claims require values";
+
+            var badRequestResponse = new BadRequestResponse()
+            {
+                Headers = new HeaderDictionary(),
+                HttpStatusCode = HttpStatusCode.Ambiguous,
+                Response = response,
+                BadRequestResponseOverride = (claims, invalidClaims) =>
+                {
+                    return Task.FromResult((HttpResponse)new DefaultHttpResponse(new DefaultHttpContext()
+                    {
+                        Response =
+                        {
+                            Body = new MemoryStream(Encoding.UTF8.GetBytes("yo this is awesome")),
+                            StatusCode = 500,
+                        }
+                    }));
+                }
+            };
+            Assert.Equal(HttpStatusCode.Ambiguous, badRequestResponse.HttpStatusCode);
+            Assert.Equal(response, badRequestResponse.Response);
+            Assert.NotNull(badRequestResponse.BadRequestResponseOverride);
+            var badRequestResponseOverrideResponse = badRequestResponse.BadRequestResponseOverride.Invoke(new List<string>(), new List<InvalidClaimResult>());
+            Assert.Equal(badRequestResponseOverrideResponse.Result.StatusCode, (int)HttpStatusCode.InternalServerError);
+            var responseByteArray = new byte[badRequestResponseOverrideResponse.Result.Body.Length];
+            badRequestResponseOverrideResponse.Result.Body.Read(responseByteArray, 0, (int) badRequestResponseOverrideResponse.Result.Body.Length);
+            Assert.Equal("yo this is awesome", Encoding.UTF8.GetString(responseByteArray));
+        }
+    }
+}
