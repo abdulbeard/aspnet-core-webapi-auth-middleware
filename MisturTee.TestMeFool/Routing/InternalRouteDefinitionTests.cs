9f577ec53b3e21a5814a7182a93800988a21e1c6
AbdulBeard <abdulkhaliqzaheer@gmail.com>
2018-03-19 15:52:51 -0400

    adding unit tests etc.

---
+++ MisturTee.TestMeFool/Routing/InternalRouteDefinitionTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using MisturTee.Config;
+using MisturTee.Config.Claims;
+using MisturTee.Config.Claims.ExtractionConfigs;
+using MisturTee.Config.Claims.ExtractionConfigs.Valid;
+using MisturTee.Config.Routing;
+using Xunit;
+
+namespace MisturTee.TestMeFool.Routing
+{
+    public class InternalRouteDefinitionTests
+    {
+        [Fact]
+        public void InternalRouteDefinition()
+        {
+            var routeDef = new PostRouteDefinition(typeof(TestingType));
+            routeDef.ClaimsConfig = new RouteClaimsConfig()
+            {
+                ExtractionConfigs = new List<IValidClaimsExtractionConfig>()
+                {
+                    new JsonPathClaimExtractionConfig("YoloClaim")
+                        .ConfigureExtraction(ExtractionFunctions.JsonPathFunc, "$.json").Build()
+                }
+            };
+            routeDef.RouteTemplate = "/who/letThe/Dogs/out/{culpritId}";
+            var internalRouteDef = new InternalRouteDefinition(routeDef);
+            Assert.Equal(HttpMethod.Post, internalRouteDef.Method);
+            Assert.Equal(typeof(TestingType), internalRouteDef.RequestBody);
+        }
+    }
+}
