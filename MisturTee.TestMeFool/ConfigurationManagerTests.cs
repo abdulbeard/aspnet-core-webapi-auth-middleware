7ec799c55ff4c3a68465bbf5e8dea4b226bb5c02
AbdulBeard <abdulkhaliqzaheer@gmail.com>
2018-03-19 22:36:26 -0400

    more tests

---
+++ MisturTee.TestMeFool/ConfigurationManagerTests.cs
@@ -0,0 +1,43 @@
+﻿using MisturTee.Config;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace MisturTee.TestMeFool
+{
+    public class ConfigurationManagerTests
+    {
+        [Fact]
+        public void Appsettings()
+        {
+            var loglevelCount = new ConfigurationManager().Appsettings<int>("Logging:Console:LogLevel:Count");
+            Assert.Equal(54, loglevelCount);
+        }
+
+        [Fact]
+        public void Appsettings_ChangeTypeException()
+        {
+            var loglevelDefault = new ConfigurationManager().Appsettings<int>("Logging:Console:LogLevel:Default");
+            Assert.Equal(default(int), loglevelDefault);
+        }
+
+        [Fact]
+        public void Appsettings_JsonAppsetting()
+        {
+            var testingType = new TestingType() { Yo = "lo", No = "lo" };
+            var testingTypeDeserialized = new ConfigurationManager().JsonAppsetting<TestingType>("testingType");
+            Assert.Equal(testingType.Yo, testingTypeDeserialized.Yo);
+            Assert.Equal(testingType.No, testingTypeDeserialized.No);
+        }
+
+        [Fact]
+        public void Appsettings_JsonAppsetting_InvalidJson()
+        {
+            var testingType = new TestingType() { Yo = "lo", No = "lo" };
+            var testingTypeDeserialized = new ConfigurationManager().JsonAppsetting<TestingType>("invalidJson");
+            Assert.Null(testingTypeDeserialized);
+        }
+    }
+}
--- MisturTee.TestMeFool/Routing/InternalRouteDefinitionTests.cs
+++ MisturTee.TestMeFool/Routing/InternalRouteDefinitionTests.cs
@@ -25,10 +25,168 @@ namespace MisturTee.TestMeFool.Routing
                         .ConfigureExtraction(ExtractionFunctions.JsonPathFunc, "$.json").Build()
                 }
             };
-            routeDef.RouteTemplate = "/who/letThe/Dogs/out/{culpritId}";
+            routeDef.RouteTemplate = "who/letThe/Dogs/out/{culpritId}";
             var internalRouteDef = new InternalRouteDefinition(routeDef);
             Assert.Equal(HttpMethod.Post, internalRouteDef.Method);
             Assert.Equal(typeof(TestingType), internalRouteDef.RequestBody);
+        }
+
+        [Fact]
+        public void InternalRouteDefinition_BadRouteTemplate()
+        {
+            var routeDef = new PostRouteDefinition(typeof(TestingType));
+            routeDef.ClaimsConfig = new RouteClaimsConfig()
+            {
+                ExtractionConfigs = new List<IValidClaimsExtractionConfig>()
+                {
+                    new JsonPathClaimExtractionConfig("YoloClaim")
+                        .ConfigureExtraction(ExtractionFunctions.JsonPathFunc, "$.json").Build()
+                }
+            };
+            routeDef.RouteTemplate = "slkdfjlsdkjf?&lksdflkjsdflk";
+            try
+            {
+                var internalRouteDef = new InternalRouteDefinition(routeDef);
+                Assert.False(true);
+            }
+            catch (Exception) { }
+        }
+
+        [Fact]
+        public void InternalRouteDefinition_Post()
+        {
+            var routeDef = new PostRouteDefinition(typeof(TestingType));
+            var claimsConfig = new RouteClaimsConfig()
+            {
+                ExtractionConfigs = new List<IValidClaimsExtractionConfig>()
+                {
+                    new JsonPathClaimExtractionConfig("YoloClaim")
+                        .ConfigureExtraction(ExtractionFunctions.JsonPathFunc, "$.json").Build()
+                }
+            };
+            routeDef.ClaimsConfig = claimsConfig;
+            var internalRouteDef = new InternalRouteDefinition(routeDef);
+            Assert.Equal(HttpMethod.Post, internalRouteDef.Method);
+            Assert.Equal(typeof(TestingType), internalRouteDef.RequestBody);
+            Assert.Equal(claimsConfig, internalRouteDef.ClaimsConfig);
+        }
+
+        [Fact]
+        public void InternalRouteDefinition_Get()
+        {
+            var routeDef = new GetRouteDefinition();
+            var claimsConfig = new RouteClaimsConfig()
+            {
+                ExtractionConfigs = new List<IValidClaimsExtractionConfig>()
+                {
+                    new JsonPathClaimExtractionConfig("YoloClaim")
+                        .ConfigureExtraction(ExtractionFunctions.JsonPathFunc, "$.json").Build()
+                }
+            };
+            routeDef.ClaimsConfig = claimsConfig;
+            var internalRouteDef = new InternalRouteDefinition(routeDef);
+            Assert.Equal(HttpMethod.Get, internalRouteDef.Method);
+            Assert.Equal(typeof(object), internalRouteDef.RequestBody);
+            Assert.Equal(claimsConfig, internalRouteDef.ClaimsConfig);
+        }
+
+        [Fact]
+        public void InternalRouteDefinition_Put()
+        {
+            var routeDef = new PutRouteDefinition(typeof(TestingType));
+            var claimsConfig = new RouteClaimsConfig()
+            {
+                ExtractionConfigs = new List<IValidClaimsExtractionConfig>()
+                {
+                    new JsonPathClaimExtractionConfig("YoloClaim")
+                        .ConfigureExtraction(ExtractionFunctions.JsonPathFunc, "$.json").Build()
+                }
+            };
+            routeDef.ClaimsConfig = claimsConfig;
+            var internalRouteDef = new InternalRouteDefinition(routeDef);
+            Assert.Equal(HttpMethod.Put, internalRouteDef.Method);
+            Assert.Equal(typeof(TestingType), internalRouteDef.RequestBody);
+            Assert.Equal(claimsConfig, internalRouteDef.ClaimsConfig);
+        }
+
+
+        [Fact]
+        public void InternalRouteDefinition_Delete()
+        {
+            var routeDef = new DeleteRouteDefinition(typeof(TestingType));
+            var claimsConfig = new RouteClaimsConfig()
+            {
+                ExtractionConfigs = new List<IValidClaimsExtractionConfig>()
+                {
+                    new JsonPathClaimExtractionConfig("YoloClaim")
+                        .ConfigureExtraction(ExtractionFunctions.JsonPathFunc, "$.json").Build()
+                }
+            };
+            routeDef.ClaimsConfig = claimsConfig;
+            var internalRouteDef = new InternalRouteDefinition(routeDef);
+            Assert.Equal(HttpMethod.Delete, internalRouteDef.Method);
+            Assert.Equal(typeof(TestingType), internalRouteDef.RequestBody);
+            Assert.Equal(claimsConfig, internalRouteDef.ClaimsConfig);
+        }
+
+
+        [Fact]
+        public void InternalRouteDefinition_Head()
+        {
+            var routeDef = new HeadRouteDefinition();
+            var claimsConfig = new RouteClaimsConfig()
+            {
+                ExtractionConfigs = new List<IValidClaimsExtractionConfig>()
+                {
+                    new JsonPathClaimExtractionConfig("YoloClaim")
+                        .ConfigureExtraction(ExtractionFunctions.JsonPathFunc, "$.json").Build()
+                }
+            };
+            routeDef.ClaimsConfig = claimsConfig;
+            var internalRouteDef = new InternalRouteDefinition(routeDef);
+            Assert.Equal(HttpMethod.Head, internalRouteDef.Method);
+            Assert.Equal(typeof(object), internalRouteDef.RequestBody);
+            Assert.Equal(claimsConfig, internalRouteDef.ClaimsConfig);
+        }
+
+
+        [Fact]
+        public void InternalRouteDefinition_Options()
+        {
+            var routeDef = new OptionsRouteDefinition();
+            var claimsConfig = new RouteClaimsConfig()
+            {
+                ExtractionConfigs = new List<IValidClaimsExtractionConfig>()
+                {
+                    new JsonPathClaimExtractionConfig("YoloClaim")
+                        .ConfigureExtraction(ExtractionFunctions.JsonPathFunc, "$.json").Build()
+                }
+            };
+            routeDef.ClaimsConfig = claimsConfig;
+            var internalRouteDef = new InternalRouteDefinition(routeDef);
+            Assert.Equal(HttpMethod.Options, internalRouteDef.Method);
+            Assert.Equal(typeof(object), internalRouteDef.RequestBody);
+            Assert.Equal(claimsConfig, internalRouteDef.ClaimsConfig);
+        }
+
+
+        [Fact]
+        public void InternalRouteDefinition_Trace()
+        {
+            var routeDef = new TraceRouteDefinition();
+            var claimsConfig = new RouteClaimsConfig()
+            {
+                ExtractionConfigs = new List<IValidClaimsExtractionConfig>()
+                {
+                    new JsonPathClaimExtractionConfig("YoloClaim")
+                        .ConfigureExtraction(ExtractionFunctions.JsonPathFunc, "$.json").Build()
+                }
+            };
+            routeDef.ClaimsConfig = claimsConfig;
+            var internalRouteDef = new InternalRouteDefinition(routeDef);
+            Assert.Equal(HttpMethod.Trace, internalRouteDef.Method);
+            Assert.Equal(typeof(object), internalRouteDef.RequestBody);
+            Assert.Equal(claimsConfig, internalRouteDef.ClaimsConfig);
         }
     }
 }
