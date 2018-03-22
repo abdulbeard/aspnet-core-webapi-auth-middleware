46e05dcf3f1157a45b650ac3a6152c4670a31e82
AbdulBeard <abdulkhaliqzaheer@gmail.com>
2018-03-22 19:46:41 -0400

    Middleware extensions tests

--- MisturTee.TestMeFool/Middleware/MiddlewareExtensionsTests.cs
+++ MisturTee.TestMeFool/Middleware/MiddlewareExtensionsTests.cs
@@ -1,6 +1,41 @@
-﻿namespace MisturTee.TestMeFool.Middleware
+﻿using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder.Internal;
+using MisturTee.Config;
+using MisturTee.Config.Routing;
+using MisturTee.Middleware;
+using Xunit;
+
+namespace MisturTee.TestMeFool.Middleware
 {
     public class MiddlewareExtensionsTests
     {
+        [Fact]
+        public void UseCustomClaimsValidationAsync_RouteDefinitions()
+        {
+            var applicationBuilder = new ApplicationBuilder(new ServiceContainer());
+            var appBuilder = applicationBuilder.UseCustomClaimsValidationAsync(new List<IRouteDefinitions>())
+                .Result;
+            Assert.NotNull(appBuilder);
+        }
+
+
+        [Fact]
+        public void UseCustomClaimsValidationAsync_RoutesProvider()
+        {
+            var applicationBuilder = new ApplicationBuilder(new ServiceContainer());
+            var appBuilder = applicationBuilder
+                .UseCustomClaimsValidationAsync(new TestValidRouteDefinitionProvider()).Result;
+            Assert.NotNull(appBuilder);
+        }
+
+        private class TestValidRouteDefinitionProvider : IValidRouteDefinitionProvider
+        {
+            public Task<IEnumerable<IRouteDefinitions>> GetAsync()
+            {
+                return null;
+            }
+        }
     }
 }
