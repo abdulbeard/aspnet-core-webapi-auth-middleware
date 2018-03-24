3422f84a5a67c72e0579ee8bc44c12c5feb3c784
AbdulBeard <abdulkhaliqzaheer@gmail.com>
2018-03-23 22:53:33 -0400

    fixing up slashes

--- BuildTools/OpenCover/CodeCoverage.ps1
+++ BuildTools/OpenCover/CodeCoverage.ps1
@@ -1,2 +1,2 @@
 Write-Host (Get-Item -Path ".\" -Verbose).FullName;
-.\OpenCover.Console.exe -target:"C:/Program Files/dotnet/dotnet.exe" -targetargs:"test ..\..\MisturTee.TestMeFool\MisturTee.TestMeFool.csproj --configuration Debug --no-build" -filter:"+[*]* -[*.Test*]*" -oldStyle -register:user -output:"..\..\OpenCover.xml";
+.\OpenCover.Console.exe -target:"C:\Program Files\dotnet\dotnet.exe" -targetargs:"test ..\..\MisturTee.TestMeFool\MisturTee.TestMeFool.csproj --configuration Debug --no-build" -filter:"+[*]* -[*.Test*]*" -oldStyle -register:user -output:"..\..\OpenCover.xml";
