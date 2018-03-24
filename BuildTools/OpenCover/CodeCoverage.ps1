b6c9ae57c54e001081a7c0656fcd762fa116d080
AbdulBeard <abdulkhaliqzaheer@gmail.com>
2018-03-23 22:38:44 -0400

    more changes

--- BuildTools/OpenCover/CodeCoverage.ps1
+++ BuildTools/OpenCover/CodeCoverage.ps1
@@ -1,1 +1,2 @@
-OpenCover.Console.exe -target:"C:/Program Files/dotnet/dotnet.exe" -targetargs:"test ..\..\MisturTee.TestMeFool\MisturTee.TestMeFool.csproj --configuration Debug --no-build" -filter:"+[*]* -[*.Test*]*" -oldStyle -register:user -output:"OpenCover.xml";
+echo pwd;
+OpenCover.Console.exe -target:"C:/Program Files/dotnet/dotnet.exe" -targetargs:"test ..\..\MisturTee.TestMeFool\MisturTee.TestMeFool.csproj --configuration Debug --no-build" -filter:"+[*]* -[*.Test*]*" -oldStyle -register:user -output:"..\..\OpenCover.xml";
--- BuildTools/OpenCoverToCobertura.ps1
+++ BuildTools/OpenCoverToCobertura.ps1
@@ -1,1 +1,2 @@
+echo pwd;
 OpenCoverToCoberturaConverter.exe -Wait -NoNewWindow -ArgumentList "-input:'..\OpenCover.xml'" -output:"..\Cobertura.xml" -sources:"..\";
