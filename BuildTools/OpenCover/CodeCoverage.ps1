09f565aa2a4752866291fa5b7a8fe634229e02e3
AbdulBeard <abdulkhaliqzaheer@gmail.com>
2018-03-23 23:22:28 -0400

    testing the script

--- BuildTools/OpenCover/CodeCoverage.ps1
+++ BuildTools/OpenCover/CodeCoverage.ps1
@@ -1,3 +1,4 @@
 Write-Host (Get-Item -Path ".\" -Verbose).FullName;
 .\OpenCover.Console.exe 
+.\OpenCover.Console.exe -target:"C:/Program Files/dotnet/dotnet.exe" -targetargs:"test ..\..\MisturTee.TestMeFool\MisturTee.TestMeFool.csproj --configuration Debug --no-build"
 .\OpenCover.Console.exe -target:"C:\Program Files\dotnet\dotnet.exe" -targetargs:"test ..\..\MisturTee.TestMeFool\MisturTee.TestMeFool.csproj --configuration Debug --no-build" -filter:"+[*]* -[*.Test*]*" -oldStyle -register:user -output:"..\..\OpenCover.xml";
