3d22b904de8d7e1d546aea05e7622a3de91aa69b
AbdulBeard <abdulkhaliqzaheer@gmail.com>
2018-03-24 00:12:56 -0400

    tryna write to host

--- BuildTools/OpenCover/CodeCoverage.ps1
+++ BuildTools/OpenCover/CodeCoverage.ps1
@@ -1,4 +1,4 @@
 Write-Host (Get-Item -Path ".\" -Verbose).FullName;
-.\OpenCover.Console.exe 
-.\OpenCover.Console.exe -target:"C:/Program Files/dotnet/dotnet.exe" -targetargs:"test ..\..\MisturTee.TestMeFool\MisturTee.TestMeFool.csproj --configuration Debug --no-build" -filter:"+[*]* -[*.Test*]*"
-.\OpenCover.Console.exe -target:"C:/Program Files/dotnet/dotnet.exe" -targetargs:"test ..\..\MisturTee.TestMeFool\MisturTee.TestMeFool.csproj --configuration Debug --no-build" -filter:"+[*]* -[*.Test*]*" -oldStyle -register:user -output:"..\..\OpenCover.xml";
+#.\OpenCover.Console.exe 
+.\OpenCover.Console.exe -target:"C:/Program Files/dotnet/dotnet.exe" -targetargs:"test ..\..\MisturTee.TestMeFool\MisturTee.TestMeFool.csproj --configuration Debug --no-build" -filter:"+[*]* -[*.Test*]*" -oldStyle -register:user -output:"..\..\OpenCover.xml" | Write-Host;
+#.\OpenCover.Console.exe -target:"C:/Program Files/dotnet/dotnet.exe" -targetargs:"test ..\..\MisturTee.TestMeFool\MisturTee.TestMeFool.csproj --configuration Debug --no-build" -filter:"+[*]* -[*.Test*]*" -oldStyle -register:user -output:"..\..\OpenCover.xml";
