a9ff5151a64e127a92dbb706e810ad8afa3f9a2e
AbdulBeard <abdulkhaliqzaheer@gmail.com>
2018-03-23 22:19:02 -0400

    fixing up the script again

---
+++ BuildTools/OpenCover/CodeCoverage.ps1
@@ -0,0 +1,1 @@
+OpenCover.Console.exe -target:"C:/Program Files/dotnet/dotnet.exe" -targetargs:"test ..\..\MisturTee.TestMeFool\MisturTee.TestMeFool.csproj --configuration Debug --no-build" -filter:"+[*]* -[*.Test*]*" -oldStyle -register:user -output:"OpenCover.xml";
