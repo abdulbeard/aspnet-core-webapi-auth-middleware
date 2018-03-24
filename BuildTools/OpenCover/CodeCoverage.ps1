781612e4643b5314ad3da77557008a0af6a25383
AbdulBeard <abdulkhaliqzaheer@gmail.com>
2018-03-24 00:46:07 -0400

    removing register:user

--- BuildTools/OpenCover/CodeCoverage.ps1
+++ BuildTools/OpenCover/CodeCoverage.ps1
@@ -6,5 +6,5 @@ Write-Host $sourcesDirectory
 $filter = "+[*]* -[*.Test*]*";
 Write-Host $filter;
 #.\OpenCover.Console.exe 
-.\OpenCover.Console.exe -target:"C:/Program Files/dotnet/dotnet.exe" -targetargs:"test ..\..\MisturTee.TestMeFool\MisturTee.TestMeFool.csproj --configuration Debug --no-build" -filter:$filter -oldStyle -register:user -output:"$sourcesDirectory\OpenCover.xml" | Write-Host;
+.\OpenCover.Console.exe -target:"C:/Program Files/dotnet/dotnet.exe" -targetargs:"test ..\..\MisturTee.TestMeFool\MisturTee.TestMeFool.csproj --configuration Debug --no-build" -filter:$filter -oldStyle -register -output:"$sourcesDirectory\OpenCover.xml" | Write-Host;
 #.\OpenCover.Console.exe -target:"C:/Program Files/dotnet/dotnet.exe" -targetargs:"test ..\..\MisturTee.TestMeFool\MisturTee.TestMeFool.csproj --configuration Debug --no-build" -filter:"+[*]* -[*.Test*]*" -oldStyle -register:user -output:"..\..\OpenCover.xml";
